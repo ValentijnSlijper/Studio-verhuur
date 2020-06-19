a120a2583c000637d307b1e68fd988d5ff330065
FreshHD <99057635@mydavinci.nl>
2020-06-19 15:44:02 +0200

    **Fixed join to view discription at reservation/detail **

--- model/ReservationModel.php
+++ model/ReservationModel.php
@@ -20,12 +20,12 @@ function selectReservation($id){
   try {
     $conn = openDatabaseConnection();
     $stmt = $conn->prepare("
-    	SELECT r.id, s.name AS studio, s.img AS studioimg, u.name AS user, r.price, r.user, r.starttime, r.endtime, i.name AS instrument, i.img AS instrumentimg 
-    	FROM reservations r 
-    	JOIN instruments i ON i.id = r.instruments 
-    	JOIN studios s ON r.studio = s.id 
-    	JOIN users u ON r.user = u.id 
-    	WHERE r.id = 1"
+    	SELECT s.description ,r.id, s.name AS studio, s.img AS studioimg, u.name AS user, r.price, r.starttime, r.endtime, i.name AS instrument, i.img AS instrumentimg
+    	FROM reservations r
+    	JOIN instruments i ON i.id = r.instruments
+    	JOIN studios s ON r.studio = s.id
+    	JOIN users u ON r.user = u.id
+    	WHERE r.id = :id"
     );
     $stmt->bindParam(':id', $id);
     $stmt->execute();
